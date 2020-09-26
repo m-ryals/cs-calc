82ea530a36426e2aeb02bee32c39e87bae64bad0
elilowe <elilowe@192.168.1.23>
2020-09-25 20:51:23 -0400

    Comments

--- CalculatorQ1.java
+++ CalculatorQ1.java
@@ -1,9 +1,15 @@
+/**
+ * Title: Calculator
+ * Authors: Eli Lowe and Matthew Ryals
+ * Description: This program calculates user inputs
+ */
+
 public class CalculatorQ1 {
-	
+	//Prints a message when program starts
 	public void startup() {
         System.out.println("Welcome to our Calculator! Good luck on your math!");
     }
-    
+    //Prints a message when program ends
     public void exit(int code) {
         System.out.println("Thanks for using our Calculator! Goodbye!");
         System.exit(code);
