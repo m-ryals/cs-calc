de3d4d84586a5a7a6e3db93460d6ad9623c7fea2
m-ryals <matthew.a.ryals@gmail.com>
2020-09-25 20:39:59 -0400

    add startup and exit methods

--- CalculatorQ1.java
+++ CalculatorQ1.java
@@ -2,4 +2,13 @@ public class CalculatorQ1 {
 	public static void main(String[] args) {
 		
 	}
+	
+	public void startup() {
+		System.out.println("Welcome to our Calculator! Good luck on your math!");
+	}
+	
+	public void exit(int code) {
+		System.out.println("Thanks for using our Calculator! Goodbye!");
+		System.exit(code);
+	}
 }
