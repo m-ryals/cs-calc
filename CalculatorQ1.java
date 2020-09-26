559e8f183d122a21ee7e425742e5731788c9475a
m-ryals <matthew.a.ryals@gmail.com>
2020-09-25 20:44:42 -0400

    committt

--- CalculatorQ1.java
+++ CalculatorQ1.java
@@ -13,12 +13,4 @@ public class CalculatorQ1 {
 		
 	}
 	
-	public void startup() {
-		System.out.println("Welcome to our Calculator! Good luck on your math!");
-	}
-	
-	public void exit(int code) {
-		System.out.println("Thanks for using our Calculator! Goodbye!");
-		System.exit(code);
-	}
 }
